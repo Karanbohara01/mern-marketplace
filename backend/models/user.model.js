39bf05e8d9efb27407a169a3fe8cfc4bd918f22d
Acer <karanbohara216@gmail.com>
2025-01-13 01:08:48 +0545

    user-schema-modified

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -11,6 +11,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     require: true,
   },
+  email: {
+    type: String,
+    require: true,
+  },
 });
 
 const User = new mongoose.model("User", userSchema);
